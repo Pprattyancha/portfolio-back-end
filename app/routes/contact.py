200a77354d35187b7847a406d92a919dce45bee3
unknown <Pprattyancha>
2026-07-11 16:46:10 +0530

    send-email

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -5,5 +5,5 @@ from app.services.contact_service import add_contact
 router = APIRouter()
 
 @router.post("/add-contact")
-def create_contact(contact: Contact, background_tasks: BackgroundTasks):
-    return add_contact(contact, background_tasks)
+async def create_contact(contact: Contact, background_tasks: BackgroundTasks):
+    return await add_contact(contact, background_tasks)
