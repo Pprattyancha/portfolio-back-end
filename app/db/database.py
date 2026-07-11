864ca644696a8d439ad84cd1833e613ca2b62983
unknown <Pprattyancha>
2026-07-11 14:34:31 +0530

    new func

--- app/db/database.py
+++ app/db/database.py
@@ -1,14 +1,9 @@
-import mysql.connector
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "127.0.0.1"),
+        host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
-        port=3306
+        port=3306,
+        ssl_disabled=False   # 👈 IMPORTANT
     )
