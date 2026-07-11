6383bba17bd7784154d541123e804193eb0e1101
unknown <Pprattyancha>
2026-07-11 15:58:32 +0530

    task

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter
-from app.models.contact_model import Contact
+from fastapi import APIRouter, BackgroundTasks
 from app.services.contact_service import add_contact
 
 router = APIRouter()
 
 @router.post("/add-contact")
-def create_contact(contact: Contact):
-    return add_contact(contact)
+def create_contact(contact, background_tasks: BackgroundTasks):
+    return add_contact(contact, background_tasks)
